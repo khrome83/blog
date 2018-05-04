9efacaa8a38694b3bba1fab6131c831f3a0f2874
Zane Milakovic <zane.milakovic@icloud.com>
2018-05-04 06:29:35 -0400

    removed cached contents

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/zane/GitHub/blog/src/layouts/index.js"
-  import data from "/Users/zane/GitHub/blog/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
