2a96929dc411b324f33dca3c37f6a4c2aec48949
Zane Milakovic <zane.milakovic@icloud.com>
2018-05-05 11:51:15 -0400

    adds markdown render for blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,20 @@
 module.exports = {
   siteMetadata: {
     title: 'Zane Milakovic\'s Terrible Code Blog',
+    author: 'Zane Milakovic',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-jsx-postcss',
     'gatsby-plugin-eslint',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,63 @@
 import React from "react";
 import Link from "gatsby-link";
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+
+    <div>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div className="post" key={node.id}>
+          <Link to={node.fields.slug} className="post-link">
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span className="date">— {node.frontmatter.date}</span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <style jsx>{`
+      .post {
+        margin-bottom: 2rem;
+      }
+
+      .post-link {
+        text-decoration: none;
+        color: inherit;
+      }
+
+      .date {
+        color: #f0f0f0;
+      }
+    `}</style>
   </div>
 );
 
 export default IndexPage;
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
