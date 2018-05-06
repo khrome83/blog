1d0b1264dae53d97d675f10e8bf588ef15431722
Zane Milakovic <zane.milakovic@icloud.com>
2018-05-06 09:20:58 -0400

    reenables mailgun

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,7 +6,7 @@ export default ({ data }) => {
     <div>
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      <form method="POST" action="https://api.staticman.net/v2/entry/khrome83/blog/master">
+      <form method="POST" action="https://api.staticman.net/v2/entry/khrome83/blog/master/comments">
         <input name="options[redirect]" type="hidden" value="localhost:8000" />
         <input name="options[slug]" type="hidden" value={post.fields.slug.slice(1, -1)} />
         <label><input name="fields[name]" type="text" />Name</label>
