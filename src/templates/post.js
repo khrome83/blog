5f6895029be358597cdbc20359869b607559aca7
Zane Milakovic <zane.milakovic@icloud.com>
2018-05-06 09:03:16 -0400

    small changes to work through issues with staticman

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,9 +6,9 @@ export default ({ data }) => {
     <div>
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      <form method="POST" action="https://api.staticman.net/v2/entry/khrome83/blog/master/comments">
-        <input name="options[redirect]" type="hidden" value="https://zanemilakovic.com" />
-        <input name="options[slug]" type="hidden" value={post.fields.slug} />
+      <form method="POST" action="https://api.staticman.net/v2/entry/khrome83/blog/master">
+        <input name="options[redirect]" type="hidden" value="localhost:8000" />
+        <input name="options[slug]" type="hidden" value={post.fields.slug.slice(1, -1)} />
         <label><input name="fields[name]" type="text" />Name</label>
         <label><input name="fields[email]" type="email" />E-mail</label>
         <label><textarea name="fields[message]"></textarea>Message</label>
